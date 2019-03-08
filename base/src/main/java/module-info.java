9280d7ffa74c82622e9b413d0be880b1f1f0a423
abondar24 <abondar1992@gmail.com>
2019-03-08 01:40:48 +0100

    Mapper scan fix

--- base/src/main/java/module-info.java
+++ base/src/main/java/module-info.java
@@ -5,7 +5,10 @@ module org.abondar.experimental.wsboard.base {
    requires org.abondar.experimental.wsboard.dataModel;
    requires org.mybatis;
    requires org.mybatis.spring;
+   requires spring.context;
 
    opens org.abondar.experimental.wsboard.base;
+   opens org.abondar.experimental.wsboard.base.dao;
+
 
 }
--- base/src/main/java/org/abondar/experimental/wsboard/base/Main.java
+++ base/src/main/java/org/abondar/experimental/wsboard/base/Main.java
@@ -1,13 +1,26 @@
 package org.abondar.experimental.wsboard.base;
 
 
+import org.abondar.experimental.wsboard.base.dao.DataMapper;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.mapper.MapperFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@MapperScan({"org.abondar.experimental.wsboard.base.dao"})
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
+    }
+
+    @Bean
+    public DataMapper mapper(SqlSessionFactory sqlSessionFactory){
+        SqlSessionTemplate sessionTemplate = new SqlSessionTemplate( sqlSessionFactory );
+        return sessionTemplate.getMapper(DataMapper.class);
     }
 }
