31b71c304ef1fcbbd71a8ef63e42cf6d329774c0
abondar24 <abondar1992@gmail.com>
2019-04-11 21:14:27 +0200

    contributor dao refactoring

--- base/src/test/java/org/abondar/experimental/wsboard/base/data/dao/ContributorDaoTest.java
+++ base/src/test/java/org/abondar/experimental/wsboard/base/data/dao/ContributorDaoTest.java
@@ -4,6 +4,9 @@ import org.abondar.experimental.wsboard.base.Main;
 import org.abondar.experimental.wsboard.base.data.DataMapper;
 import org.abondar.experimental.wsboard.base.data.ErrorMessageUtil;
 import org.abondar.experimental.wsboard.base.data.MapperTest;
+import org.abondar.experimental.wsboard.base.data.ObjectWrapper;
+import org.abondar.experimental.wsboard.datamodel.Project;
+import org.abondar.experimental.wsboard.datamodel.User;
 import org.abondar.experimental.wsboard.datamodel.UserRole;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,7 +22,8 @@ import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = Main.class)
 @ExtendWith(SpringExtension.class)
@@ -45,28 +49,15 @@ public class ContributorDaoTest {
     public void createContributorTest() throws Exception {
         logger.info("Create contributor test");
 
-        var login = "login";
-        var email = "email@email.com";
-        var password = "pwd";
-        var firstName = "fname";
-        var lastName = "lname";
-        var roles = List.of(UserRole.Developer.name(), UserRole.DevOps.name());
-
-        var usr = userDao.createUser(login, password, email, firstName, lastName, roles);
-
-        var name = "test";
-        var startDate = new Date();
-        var prj = projectDao.createProject(name, startDate);
-        prj = projectDao.updateProject(prj.getObject().getId(), null, null, true, null);
+        var usr = createUser();
+        var prj = createProject(true);
 
         var contr = contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), true);
 
         assertNull(contr.getMessage());
         assertTrue(contr.getObject().getId() > 0);
 
-        mapper.deleteContributors();
-        mapper.deleteProjects();
-        mapper.deleteUsers();
+        cleanData();
     }
 
 
@@ -74,28 +65,15 @@ public class ContributorDaoTest {
     public void createContributorNotOwnerTest() throws Exception {
         logger.info("Create contributor not owner test");
 
-        var login = "login";
-        var email = "email@email.com";
-        var password = "pwd";
-        var firstName = "fname";
-        var lastName = "lname";
-        var roles = List.of(UserRole.Developer.name(), UserRole.DevOps.name());
-
-        var usr = userDao.createUser(login, password, email, firstName, lastName, roles);
-
-        var name = "test";
-        var startDate = new Date();
-        var prj = projectDao.createProject(name, startDate);
-        prj = projectDao.updateProject(prj.getObject().getId(), null, null, true, null);
+        var usr = createUser();
+        var prj = createProject(true);
 
         var contr = contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), false);
 
         assertNull(contr.getMessage());
         assertTrue(contr.getObject().getId() > 0);
 
-        mapper.deleteContributors();
-        mapper.deleteProjects();
-        mapper.deleteUsers();
+        cleanData();
     }
 
 
@@ -103,56 +81,31 @@ public class ContributorDaoTest {
     public void createContributorActiveFalseTest() throws Exception {
         logger.info("Create contributor active false test");
 
-        var login = "login";
-        var email = "email@email.com";
-        var password = "pwd";
-        var firstName = "fname";
-        var lastName = "lname";
-        var roles = List.of(UserRole.Developer.name(), UserRole.DevOps.name());
-
-        var usr = userDao.createUser(login, password, email, firstName, lastName, roles);
-
-        var name = "test";
-        var startDate = new Date();
-        var prj = projectDao.createProject(name, startDate);
+        var usr = createUser();
+        var prj = createProject(false);
 
         var contr = contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), false);
 
-        assertEquals(ErrorMessageUtil.PROJECT_NOT_ACTIVE,contr.getMessage());
+        assertEquals(ErrorMessageUtil.PROJECT_NOT_ACTIVE, contr.getMessage());
         assertNull(contr.getObject());
 
-        mapper.deleteContributors();
-        mapper.deleteProjects();
-        mapper.deleteUsers();
+        cleanData();
     }
 
     @Test
     public void createContributorProjectHasOwnerTest() throws Exception {
         logger.info("Create contributor project has owner test");
 
-        var login = "login";
-        var email = "email@email.com";
-        var password = "pwd";
-        var firstName = "fname";
-        var lastName = "lname";
-        var roles = List.of(UserRole.Developer.name(), UserRole.DevOps.name());
-
-        var usr = userDao.createUser(login, password, email, firstName, lastName, roles);
-
-        var name = "test";
-        var startDate = new Date();
-        var prj = projectDao.createProject(name, startDate);
-        prj = projectDao.updateProject(prj.getObject().getId(), null, null, true, null);
+        var usr = createUser();
+        var prj = createProject(true);
 
         var contr = contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), true);
         contr = contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), true);
 
-        assertEquals(ErrorMessageUtil.PROJECT_HAS_OWNER,contr.getMessage());
+        assertEquals(ErrorMessageUtil.PROJECT_HAS_OWNER, contr.getMessage());
         assertNull(contr.getObject());
 
-        mapper.deleteContributors();
-        mapper.deleteProjects();
-        mapper.deleteUsers();
+        cleanData();
     }
 
 
@@ -160,28 +113,15 @@ public class ContributorDaoTest {
     public void updateContributorAsOwnerTest() throws Exception {
         logger.info("Update contributor as owner test");
 
-        var login = "login";
-        var email = "email@email.com";
-        var password = "pwd";
-        var firstName = "fname";
-        var lastName = "lname";
-        var roles = List.of(UserRole.Developer.name(), UserRole.DevOps.name());
-
-        var usr = userDao.createUser(login, password, email, firstName, lastName, roles);
-
-        var name = "test";
-        var startDate = new Date();
-        var prj = projectDao.createProject(name, startDate);
-        prj = projectDao.updateProject(prj.getObject().getId(), null, null, true, null);
+        var usr = createUser();
+        var prj = createProject(true);
 
         var contr = contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), false);
-        contr = contributorDao.updateContributorAsOwner(contr.getObject().getId(), true,true);
+        contr = contributorDao.updateContributorAsOwner(contr.getObject().getId(), true, true);
 
         assertNull(contr.getMessage());
 
-        mapper.deleteContributors();
-        mapper.deleteProjects();
-        mapper.deleteUsers();
+        cleanData();
     }
 
 
@@ -189,136 +129,97 @@ public class ContributorDaoTest {
     public void updateContributorAsOwnerContributorNotExistsTest() {
         logger.info("Update contributor as owner contributor not exists");
 
-        var contr = contributorDao.updateContributorAsOwner(100, false,true);
+        var contr = contributorDao.updateContributorAsOwner(100, false, true);
 
-        assertEquals(ErrorMessageUtil.CONTRIBUTOR_NOT_EXISTS,contr.getMessage());
+        assertEquals(ErrorMessageUtil.CONTRIBUTOR_NOT_EXISTS, contr.getMessage());
         assertNull(contr.getObject());
 
-        mapper.deleteContributors();
-        mapper.deleteProjects();
-        mapper.deleteUsers();
+        cleanData();
     }
 
     @Test
     public void updateContributorPrtojectHasOwnerTest() throws Exception {
         logger.info("Update contributor project has owner test");
 
-        var login = "login";
-        var email = "email@email.com";
-        var password = "pwd";
-        var firstName = "fname";
-        var lastName = "lname";
-        var roles = List.of(UserRole.Developer.name(), UserRole.DevOps.name());
-
-        var usr = userDao.createUser(login, password, email, firstName, lastName, roles);
-
-        var name = "test";
-        var startDate = new Date();
-        var prj = projectDao.createProject(name, startDate);
-        prj = projectDao.updateProject(prj.getObject().getId(), null, null, true, null);
+        var usr = createUser();
+        var prj = createProject(true);
 
         var contr = contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), true);
-        contr = contributorDao.updateContributorAsOwner(contr.getObject().getId(), true,true);
+        contr = contributorDao.updateContributorAsOwner(contr.getObject().getId(), true, true);
 
-        assertEquals(ErrorMessageUtil.PROJECT_HAS_OWNER,contr.getMessage());
+        assertEquals(ErrorMessageUtil.PROJECT_HAS_OWNER, contr.getMessage());
         assertNull(contr.getObject());
 
-        mapper.deleteContributors();
-        mapper.deleteProjects();
-        mapper.deleteUsers();
+        cleanData();
     }
 
     @Test
-    public void updateContributorPrtojectHasNoOwnerTest() throws Exception {
+    public void updateContributorProjectHasNoOwnerTest() throws Exception {
         logger.info("Update contributor project has no owner test");
 
-        var login = "login";
-        var email = "email@email.com";
-        var password = "pwd";
-        var firstName = "fname";
-        var lastName = "lname";
-        var roles = List.of(UserRole.Developer.name(), UserRole.DevOps.name());
-
-        var usr = userDao.createUser(login, password, email, firstName, lastName, roles);
-
-        var name = "test";
-        var startDate = new Date();
-        var prj = projectDao.createProject(name, startDate);
-        prj = projectDao.updateProject(prj.getObject().getId(), null, null, true, null);
+        var usr = createUser();
+        var prj = createProject(true);
 
         var contr = contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), false);
-        contr = contributorDao.updateContributorAsOwner(contr.getObject().getId(), false,true);
+        contr = contributorDao.updateContributorAsOwner(contr.getObject().getId(), false, true);
 
-        assertEquals(ErrorMessageUtil.PROJECT_HAS_NO_OWNER,contr.getMessage());
+        assertEquals(ErrorMessageUtil.PROJECT_HAS_NO_OWNER, contr.getMessage());
         assertNull(contr.getObject());
 
-        mapper.deleteContributors();
-        mapper.deleteProjects();
-        mapper.deleteUsers();
+        cleanData();
     }
 
     @Test
     public void updateContributorNullTest() throws Exception {
         logger.info("update contributor null test");
 
-        var login = "login";
-        var email = "email@email.com";
-        var password = "pwd";
-        var firstName = "fname";
-        var lastName = "lname";
-        var roles = List.of(UserRole.Developer.name(), UserRole.DevOps.name());
-
-        var usr = userDao.createUser(login, password, email, firstName, lastName, roles);
-
-        var name = "test";
-        var startDate = new Date();
-        var prj = projectDao.createProject(name, startDate);
-        prj = projectDao.updateProject(prj.getObject().getId(), null, null, true, null);
+        var usr = createUser();
+        var prj = createProject(true);
 
         var contr = contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), false);
         contr = contributorDao.updateContributorAsOwner(contr.getObject().getId(), null, null);
 
         assertNull(contr.getMessage());
 
-        mapper.deleteContributors();
-        mapper.deleteProjects();
-        mapper.deleteUsers();
+        cleanData();
     }
 
 
     @Test
-    public void findProjectOwnerTest() throws Exception{
+    public void findProjectOwnerTest() throws Exception {
         logger.info("Find project owner test");
 
-        var login = "login";
-        var email = "email@email.com";
-        var password = "pwd";
-        var firstName = "fname";
-        var lastName = "lname";
-        var roles = List.of(UserRole.Developer.name(), UserRole.DevOps.name());
-
-        var usr = userDao.createUser(login, password, email, firstName, lastName, roles);
-
-        var name = "test";
-        var startDate = new Date();
-        var prj = projectDao.createProject(name, startDate);
-        prj = projectDao.updateProject(prj.getObject().getId(), null, null, true, null);
+        var usr = createUser();
+        var prj = createProject(true);
 
         contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), true);
 
         var ownr = contributorDao.findProjectOwner(prj.getObject().getId());
 
-        assertEquals(usr.getObject().getId(),ownr.getObject().getId());
+        assertEquals(usr.getObject().getId(), ownr.getObject().getId());
 
-        mapper.deleteContributors();
-        mapper.deleteProjects();
-        mapper.deleteUsers();
+        cleanData();
     }
 
     @Test
     public void findContributorsForProjectTest() throws Exception {
         logger.info("Find contributors for project test");
 
+        var usr = createUser();
+        var prj = createProject(true);
+
+        contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), true);
+        var contrs = contributorDao.findProjectContributors(prj.getObject().getId(), 0, 1);
+
+        assertNull(contrs.getMessage());
+        assertEquals(1, contrs.getObject().size());
+
+
+        cleanData();
+    }
+
+
+    private ObjectWrapper<User> createUser() throws Exception {
         var login = "login";
         var email = "email@email.com";
         var password = "pwd";
@@ -326,20 +227,22 @@ public class ContributorDaoTest {
         var lastName = "lname";
         var roles = List.of(UserRole.Developer.name(), UserRole.DevOps.name());
 
-        var usr = userDao.createUser(login, password, email, firstName, lastName, roles);
+        return userDao.createUser(login, password, email, firstName, lastName, roles);
+    }
 
+    private ObjectWrapper<Project> createProject(boolean isActive) {
         var name = "test";
         var startDate = new Date();
+
         var prj = projectDao.createProject(name, startDate);
-        prj = projectDao.updateProject(prj.getObject().getId(), null, null, true, null);
+        if (isActive) {
+            prj = projectDao.updateProject(prj.getObject().getId(), null, null, true, null);
+        }
 
-        contributorDao.createContributor(usr.getObject().getId(), prj.getObject().getId(), true);
-        var contrs = contributorDao.findProjectContributors(prj.getObject().getId(),0,1);
+        return prj;
+    }
 
-        assertNull(contrs.getMessage());
-        assertEquals(1,contrs.getObject().size());
-
-
+    private void cleanData() {
         mapper.deleteContributors();
         mapper.deleteProjects();
         mapper.deleteUsers();
