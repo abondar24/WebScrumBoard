75db58ee6c4976e152dd95a4ad549e35f11658a5
abondar24 <abondar1992@gmail.com>
2019-05-08 01:39:36 +0200

    Exceptions javadoc

--- dao/src/main/java/org/abondar/experimental/wsboard/dao/exception/DataCreationException.java
+++ dao/src/main/java/org/abondar/experimental/wsboard/dao/exception/DataCreationException.java
@@ -1,5 +1,10 @@
 package org.abondar.experimental.wsboard.dao.exception;
 
+/**
+ * Exception thrown if data can't be created
+ *
+ * @author a.bondar
+ */
 public class DataCreationException extends Exception {
     public DataCreationException(String message) {
         super(message);
--- dao/src/main/java/org/abondar/experimental/wsboard/dao/exception/DataExistenceException.java
+++ dao/src/main/java/org/abondar/experimental/wsboard/dao/exception/DataExistenceException.java
@@ -1,5 +1,11 @@
 package org.abondar.experimental.wsboard.dao.exception;
 
+/**
+ * Exception thrown if data already exists or not found
+ *
+ * @author a.bondar
+ */
+
 public class DataExistenceException extends Exception {
     public DataExistenceException(String message) {
         super(message);
