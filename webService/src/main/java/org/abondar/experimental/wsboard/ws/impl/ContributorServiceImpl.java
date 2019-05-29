fe58644597a02db30ec55f112ff9f7f1ff03e5ed
abondar24 <abondar1992@gmail.com>
2019-05-30 01:00:46 +0200

    contributor service empty implementation

--- webService/src/main/java/org/abondar/experimental/wsboard/ws/impl/ContributorServiceImpl.java
+++ webService/src/main/java/org/abondar/experimental/wsboard/ws/impl/ContributorServiceImpl.java
@@ -1,6 +1,67 @@
 package org.abondar.experimental.wsboard.ws.impl;
 
+import org.abondar.experimental.wsboard.dao.ContributorDao;
 import org.abondar.experimental.wsboard.ws.service.ContributorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Contributor service implementation
+ */
+@Path("/contributor")
 public class ContributorServiceImpl implements ContributorService {
+
+    @Autowired
+    @Qualifier("contributorDao")
+    private ContributorDao contributorDao;
+
+    @POST
+    @Path("/create")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Override
+    public Response createContributor(@FormParam("userId") long userId,
+                                      @FormParam("projectId") long projectId,
+                                      @FormParam("isOwner") boolean isOwner) {
+
+
+        return null;
+    }
+
+    @POST
+    @Path("/update")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Override
+    public Response updateContributor(@FormParam("ctrId") long contributorId,
+                                      @FormParam("isOwner") Boolean isOwner,
+                                      @FormParam("isActive") Boolean isActive) {
+        return null;
+    }
+
+    @GET
+    @Path("/find_project_owner")
+    @Override
+    public Response findProjectOwner(@QueryParam("projectId") long projectId) {
+        return null;
+    }
+
+    @GET
+    @Path("/find_project_contributors")
+    @Override
+    public Response findProjectContributors(@QueryParam("projectId") long projectId,
+                                            @QueryParam("offset") int offset,
+                                            @QueryParam("limit") int limit) {
+        return null;
+    }
 }
--- webService/src/main/java/org/abondar/experimental/wsboard/ws/service/ContributorService.java
+++ webService/src/main/java/org/abondar/experimental/wsboard/ws/service/ContributorService.java
@@ -1,7 +1,17 @@
 package org.abondar.experimental.wsboard.ws.service;
 
+import javax.ws.rs.core.Response;
+
 /**
  * Contributor web service
  */
 public interface ContributorService extends RestService {
+
+    Response createContributor(long userId, long projectId, boolean isOwner);
+
+    Response updateContributor(long contributorId, Boolean isOwner, Boolean isActive);
+
+    Response findProjectOwner(long projectId);
+
+    Response findProjectContributors(long projectId, int offset, int limit);
 }
