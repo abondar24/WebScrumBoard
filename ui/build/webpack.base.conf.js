08e82886d7346ff5a35a741918045fcc9df1b2e2
abondar24 <abondar1992@gmail.com>
2019-07-22 00:15:58 +0200

    fixed build js

--- ui/build/webpack.base.conf.js
+++ ui/build/webpack.base.conf.js
@@ -1,14 +1,14 @@
-'use strict';
-const path = require('path');
-const utils = require('./utils');
-const config = require('../config');
-const vueLoaderConfig = require('./vue-loader.conf');
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const config = require('../config')
+const vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-const createLintingRule = () =;> ({
+const createLintingRule = () => ({
   test: /\.(js|vue)$/,
   loader: 'eslint-loader',
   enforce: 'pre',
@@ -17,7 +17,7 @@ const createLintingRule = () =;> ({
     formatter: require('eslint-friendly-formatter'),
     emitWarning: !config.dev.showEslintErrorsInOverlay
   }
-});
+})
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
@@ -40,53 +40,53 @@ module.exports = {
   },
   module: {
     rules: [
-      ...(config.dev.useEslint ? [createLintingRule()] : []),;
+      ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
-        /\.vue$/,
-        loader;: 'vue-loader',
-        options;: vueLoaderConfig
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
       },
       {
-        /\.js$/,
-        loader;: 'babel-loader',
-        include;: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
-        /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader;: 'url-loader',
-        options;: {
-          10000,
-          name;: utils.assetsPath('img/[name].[hash:7].[ext]')
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
       {
-        /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader;: 'url-loader',
-        options;: {
-          10000,
-          name;: utils.assetsPath('media/[name].[hash:7].[ext]')
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
       {
-        /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader;: 'url-loader',
-        options;: {
-          10000,
-          name;: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
     ]
   },
-  {
+  node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
-    false,
+    setImmediate: false,
     // prevent webpack from injecting mocks to Node native modules
     // that does not make sense for the client
-    dgram;: 'empty',
-    fs;: 'empty',
-    net;: 'empty',
-    tls;: 'empty',
-    child_process;: 'empty'
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
   }
 }
