ecf63f25d3818f047f80e71e08b091ac20113a3e
Bondar, Alexander <Alexander.Bondar1@Clarivate.com>
2019-09-19 13:41:22 +0200

    router redirect on auth

--- ui/src/router/router.js
+++ ui/src/router/router.js
@@ -5,28 +5,27 @@ import Login from "../components/Login";
 import Register from "../components/Register";
 import User from "../components/User";
 import PasswordReset from "../components/PasswordReset";
+import store from "../store/store"
 
 Vue.use(Router);
 
-const router = new Router({
-  routes: [
-    {path: '/', name: 'Hello', component: Hello},
-    {path: '/login',name:'Login', component: Login},
-    {path: '/register',name:'Register', component: Register},
-    {path: '/user/:id' ,name:'User', component: User},
-    {path: '/reset',name:'PasswordReset', component: PasswordReset},
-    { path: '*', redirect: '/' }
-  ]
+export default new Router({
+    routes: [
+        {path: '/', name: 'Hello', component: Hello},
+        {path: '/login', name: 'Login', component: Login},
+        {path: '/register', name: 'Register', component: Register},
+        {
+            path: '/user/:id', name: 'User', component: User,
+            beforeEnter(to, from, next) {
+                if (store.state.auth.authenticated) {
+                    next();
+                } else {
+                    next('/');
+                }
+            }
+        },
+        {path: '/reset', name: 'PasswordReset', component: PasswordReset},
+        {path: '*', redirect: '/'}
+    ]
 });
 
-
-
-// router.beforeEach((to, from, next) => {
-//   if (this.$store.state.auth.authenticated) {
-//     next();
-//   } else {
-//     next('/');
-//   }
-// });
-
-export default router;
